2607f078f110d94d55fe8125dea19c5ca32d8e9a
Alexander Miss <mail@alexandermiss.com>
2021-12-14 10:36:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
 from waitress import serve
 
 app = Flask(__name__)
 
+HOSTNAME = os.etenv('HOSTNAME', 'NO_HOST')
+
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return f"Hello World from {HOSTNAME}!"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
